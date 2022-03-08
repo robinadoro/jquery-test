70bb3f2c984aa6b19a606e2ce23dc011aa520101
Robinson Idagiza <robinadoro@gmail.com>
2022-03-08 15:16:34 +0300

    Add jquery commands

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+    jQuery('h1').click(function(){
+        alert("this is a heading tag")
+    });
+    jQuery('p').click(function(){
+        alert('this is a paragraph')
+    });
+    jQuery('img').click(function(){
+        alert('This is an image')
+    })
+})
